fa19ef184aa3e53a8c380a0a4fa1e84305c9dd93
Азалия Баймуратова <ozalea@yandex.ru>
2024-09-21 19:32:48 +0500

    добавена функция лагранжа и добавлены указатели кое-где

--- lab1_chm.cpp
+++ lab1_chm.cpp
@@ -1,16 +1,18 @@
 ﻿#include <iostream>
-#include <cmath>
 #include <vector>
-double pi = acos(-1);
+#include <iomanip>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
 
 double func1(double x) {
-    return x * x * cos(pi * x);
+    return x * x * cos(M_PI * x);
 }
 
-void grid_step(double a, double b, int n) {
+double* grid_step(double a, double b, int n) {
     double h = (b - a) / n;
-
-    std::vector<double> x(n + 1);
+    //std::vector<double> x(n + 1);
+    double* x = new double[n + 1]; //make_unique?
 
     x[0] = a;
     x[n] = b;
@@ -18,13 +20,31 @@ void grid_step(double a, double b, int n) {
     for (int i = 0; i < n; ++i) {
         x[i+1] = x[i] + h;
     }
-    for (int i = 0; i <= n; ++i) {
+    /*for (int i = 0; i <= n; ++i) {
         std::cout << x[i]<< " ";
-    }
+    }*/
+    return x;
+
 }
 
+double laGrange(double* x, double X, int n)//x-массив будет огромный
+{
+    double L = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        double Pr = 1;
+        for (int j = 0; j <= n; j++)
+        {
+            if (i != j)
+            {
+                Pr = Pr * (X - x[j]) / (x[i] - x[j]);
+            }
+        }
 
-
+        L = L + func1(x[i]) * Pr;
+    }
+    return L;
+}
 
 
 
@@ -34,17 +54,23 @@ int main()
 
     double a = 0;
     double b = 1.5;
-    grid_step(a, b, 15);
+    int n = 2;
 
+    double* first = grid_step(a, b, n);//ёмкость n+1
+    
+    double* res1 = new double[n+1]; //n+1
+    for (int i = 0; i <= n; ++i) {
+        res1[i] = func1(first[i]);
+    }
+
+    for (int i = 0; i <= 2; ++i) {
+        std::cout << res1[i] << " ";
+
+    }
+    std::cout << "\n";
+    for (int i = 0; i <= 2; ++i) {
+        std::cout << first[i] << " ";
+
+    }
+    //delete[]first;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
