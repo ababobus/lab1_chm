40d39d0df51169d127337af9bdea243fe5d46a0b
Азалия Баймуратова <ozalea@yandex.ru>
2024-09-24 21:53:45 +0500

    первое задание сделано без первого графика

--- lagrange.cpp
+++ lagrange.cpp
@@ -62,59 +62,96 @@ double laGrange(double* x, double X, int n)
 int main()
 {
     std::cout << "Hello World!\n";
-
+    std::ofstream dataFile;
     double a = 0.0;
     double b = 1.5;
-    
-    std::ifstream infile("lagrange_data.dat");
-    if (!infile.good()) {
-        std::cerr << "Ошибка: файл lagrange_data.dat не найден или пуст." << std::endl;
-        return 1;
-    }
 
-    std::ofstream dataFile("lagrange_data.dat");
+    dataFile.open("lagrange_data.txt"); //интерполяиця лагранжа
     for (int n = 1; n <= 15; n++) {
-        double* x = grid_step(a, b, n); //аргументы генерируем
-
+        double* x = grid_step(a, b, n); 
 
         for (double X = a; X <= b; X += (b - a) / n) {
             double Y = laGrange(x, X, n);
             dataFile << X << " " << Y << " " << n << "\n";
         }
-        dataFile << "\n"; 
-        delete[] x; 
+        dataFile << "\n";
+        delete[] x;
     }
     dataFile.close();
 
-    std::ofstream gnuplot("sd.gp");
-    gnuplot << "set title 'График интерполяции Лагранжа'\n";
-    gnuplot << "set xlabel 'x'\n";
-    gnuplot << "set ylabel 'y'\n";
-    gnuplot << "set grid\n";
-    gnuplot << "set xrange [0:1.5]\n";
-    gnuplot << "set yrange [-1.5:0.5]\n";
-
-   /* gnuplot << "plot ";
-    for (int i = 1; i <= 15; ++i) {
-        if (i > 1) {
-            gnuplot<< ", " << std::endl;
+    dataFile.open("deltaN.txt"); //ошибка приближения
+    int c = 100000;
+    double h = (b - a) / c;
+    int N0 = 0; 
+    double* x = grid_step(a, b, 15);
+    for (int n = 1; n <= 15; n++)
+    {
+        double delta = 0;
+        double sdelta = 0;
+        for (double i = a; i <= b; i += h)
+        {
+            delta = abs(func1(i) - laGrange(x, i, n));
+            if (sdelta < delta)
+            {
+                sdelta = delta;
+                N0 = n;
+            }
         }
-        gnuplot << "'lagrange_data.dat' index " << i - 1 << " title 'lagrange(" << i << ")' with lines lw 1 lc " << i;
-    }*/
-    /*std::string colors[] = { "red", "blue", "green", "orange", "purple",
-                            "cyan", "magenta", "brown", "pink",
-                            "yellow", "black", "violet", "seagreen",
-                            "gold", "navy", "darkgreen" };*/
-
-    
-    gnuplot << "plot ";
-    for (int i = 1; i <= 15; ++i) {
-        gnuplot << "'lagrange_data.dat' using 1:" << i + 1 << " with lines title 'lagrange(" << i << "), ";
+        //std::cout << "Delta[" << n << "] = " << sdelta << std::endl;
+        dataFile << sdelta << std::endl;
     }
-    
+    dataFile.close();
 
-    gnuplot << "\n";
+    dataFile.open("laGrangeN0.txt");//Лагранж для оптимального n 
+    for (double i = a; i <= b; i += (b - a) / N0)
+    {
+        //std::cout << "L[" << i << "] = " << laGrange(x, i, N0) << std::endl;
+        dataFile << laGrange(x, i, N0) << std::endl;
+    }
+    dataFile.close();
+
+    dataFile.open("laGrangeErrN0.txt");//ошибка приближения для оптимального N0 Лагранжем 
+    for (double i = a; i <= b; i += (b - a) / N0)
+    {
+        dataFile << abs(func1(i) - laGrange(x, i, N0)) << std::endl;//погрешность 
+        std::cout << abs(func1(i) - laGrange(x, i, N0)) << std::endl;
+    }
+    dataFile.close();
+
+
+
+
+    std::ofstream gnuplot("sd.gp");
+    gnuplot << "set grid\n";
+    //gnuplot << "set title 'График интерполяции Лагранжа'\n";
+    //gnuplot << "set xlabel 'x'\n";
+    //gnuplot << "set ylabel 'y'\n";
+    //gnuplot << "set xrange [0:1.5]\n";
+    //gnuplot << "set yrange [-1.5:0.5]\n";
+    /*gnuplot << "plot ";
+    for (int i = 0; i < 15; ++i) {
+        gnuplot << "'lagrange_data.dat' using 1:" << i + 2 << " with lines title 'lagrange(" << i << "), ";
+    }*/
+    
+    /*gnuplot << "set title 'Ошибка приближения'\n";
+    gnuplot << "set ylabel 'n'\n";
+    gnuplot << "set ylabel 'Delta'\n";
+    gnuplot << "set xrange [0:15]\n";
+    gnuplot << "set yrange [-1:15]\n";
+    gnuplot << "plot 'deltaN.txt' with lp title 'Delta'\n";*/
+
+    /*gnuplot << "set title 'Лагранж от n оптимального'\n";
+    gnuplot << "set xlabel 'x'\n";
+    gnuplot << "set ylabel 'L(x)'\n";
+    gnuplot << "set xrange [0:15]\n";
+    gnuplot << "plot 'laGrangeN0.txt' with lines title 'Lagrange Interpolation'\n";*/
+
+    /*gnuplot << "set title 'Ошибка приближения для оптимального N0 Лагранжем'\n";//по нулям
+    gnuplot << "set xlabel 'x'\n";
+    gnuplot << "set ylabel 'Ошибка'\n";
+    gnuplot << "plot 'laGrangeErrN0.txt' with lines title 'Ошибка приближения'\n";*/
+    
     gnuplot.close();
     system("gnuplot -p sd.gp");
-    //delete[]x;
+    
 }
--- sd.gp
+++ sd.gp
@@ -1,7 +1,5 @@
-set title 'График интерполяции Лагранжа'
-set xlabel 'x'
-set ylabel 'y'
 set grid
-set xrange [0:1.5]
-set yrange [-1.5:0.5]
-plot 'lagrange_data.dat' using 1:2 with lines title 'lagrange(1), 'lagrange_data.dat' using 1:3 with lines title 'lagrange(2), 'lagrange_data.dat' using 1:4 with lines title 'lagrange(3), 'lagrange_data.dat' using 1:5 with lines title 'lagrange(4), 'lagrange_data.dat' using 1:6 with lines title 'lagrange(5), 'lagrange_data.dat' using 1:7 with lines title 'lagrange(6), 'lagrange_data.dat' using 1:8 with lines title 'lagrange(7), 'lagrange_data.dat' using 1:9 with lines title 'lagrange(8), 'lagrange_data.dat' using 1:10 with lines title 'lagrange(9), 'lagrange_data.dat' using 1:11 with lines title 'lagrange(10), 'lagrange_data.dat' using 1:12 with lines title 'lagrange(11), 'lagrange_data.dat' using 1:13 with lines title 'lagrange(12), 'lagrange_data.dat' using 1:14 with lines title 'lagrange(13), 'lagrange_data.dat' using 1:15 with lines title 'lagrange(14), 'lagrange_data.dat' using 1:16 with lines title 'lagrange(15), 
+set title 'Ошибка приближения для оптимального N0 Лагранжем'
+set xlabel 'x'
+set ylabel 'Ошибка'
+plot 'laGrangeErrN0.txt' with lines title 'Ошибка приближения'
